a4ebc789ae33c64e1452bd2df546f42e98997b2c
Alexander Guy <alexander.guy@pm.me>
2024-12-24 08:43:14 +0100

    Use the new network info interface

--- aws-validator-agave-ts/index.ts
+++ aws-validator-agave-ts/index.ts
@@ -6,81 +6,11 @@ const solanaConfig = new pulumi.Config("solana");
 // AWS-specific resources are created inside.
 import { sshKey, instance } from "./aws";
 
-// For any network choice, set some base validator flags.
-const baseFlags = {
-  useSnapshotArchivesAtStartup: "when-newest",
-  fullRpcAPI: false,
-  rpcPort: 8899,
-  privateRPC: true,
-  onlyKnownRPC: true,
-  dynamicPortRange: "8002-8020",
-  gossipPort: 8001,
-  rpcBindAddress: "0.0.0.0",
-  walRecoveryMode: "skip_any_corrupted_record",
-  limitLedgerSize: 50000000,
-  blockProductionMethod: "central-scheduler",
-  fullSnapshotIntervalSlots: 1000,
-  noWaitForVoteToStartLeader: true,
-  noVoting: true,
-};
-
-// Provide some default options for known Solana networks.
-const networkOptions = {
-  devnet: {
-    environment: {
-      rpcURL: "https://api.devnet.solana.com",
-    },
-    flags: {
-      ...baseFlags,
-      entryPoint: [
-        "entrypoint.devnet.solana.com:8001",
-        "entrypoint2.devnet.solana.com:8001",
-        "entrypoint3.devnet.solana.com:8001",
-        "entrypoint4.devnet.solana.com:8001",
-        "entrypoint5.devnet.solana.com:8001",
-      ],
-      knownValidator: [
-        "dv1ZAGvdsz5hHLwWXsVnM94hWf1pjbKVau1QVkaMJ92",
-        "dv2eQHeP4RFrJZ6UeiZWoc3XTtmtZCUKxxCApCDcRNV",
-        "dv4ACNkpYPcE3aKmYDqZm9G5EB3J4MRoeE7WNDRBVJB",
-        "dv3qDFk1DTF36Z62bNvrCXe9sKATA6xvVy6A798xxAS",
-      ],
-      expectedGenesisHash: "EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG",
-    },
-  },
-  testnet: {
-    environment: {
-      rpcURL: "https://api.testnet.solana.com",
-    },
-    flags: {
-      ...baseFlags,
-      entryPoint: [
-        "entrypoint.testnet.solana.com:8001",
-        "entrypoint2.testnet.solana.com:8001",
-        "entrypoint3.testnet.solana.com:8001",
-      ],
-      knownValidator: [
-        "5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on",
-        "7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY",
-        "Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN",
-        "9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv",
-      ],
-      expectedGenesisHash: "4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY",
-    },
-  },
-};
-
-type NetworkName = keyof typeof networkOptions;
-
-function isValidName(name: string): name is NetworkName {
-  return Object.prototype.hasOwnProperty.call(networkOptions, name);
-}
-
-const networkName = solanaConfig.get("network") ?? "testnet";
-
-if (!isValidName(networkName)) {
-  throw new Error(`unknown Solana network '${networkName}' specified!`);
-}
+// Lookup information about the Solana network.
+const networkName =
+  solanaConfig.get<svmkit.solana.NetworkName>("network") ??
+  svmkit.solana.NetworkName.Testnet;
+const networkInfo = svmkit.networkinfo.getNetworkInfoOutput({ networkName });
 
 // Create some keys for this validator to use.
 const validatorKey = new svmkit.KeyPair("validator-key");
@@ -99,11 +29,32 @@ new svmkit.validator.Agave(
   {
     connection,
     version: "2.0.15-1",
+    environment: {
+      rpcURL: networkInfo.rpcURL[0],
+    },
     keyPairs: {
       identity: validatorKey.json,
       voteAccount: voteAccountKey.json,
     },
-    ...networkOptions[networkName],
+    flags: {
+      useSnapshotArchivesAtStartup: "when-newest",
+      fullRpcAPI: false,
+      rpcPort: 8899,
+      privateRPC: true,
+      onlyKnownRPC: true,
+      dynamicPortRange: "8002-8020",
+      gossipPort: 8001,
+      rpcBindAddress: "0.0.0.0",
+      walRecoveryMode: "skip_any_corrupted_record",
+      limitLedgerSize: 50000000,
+      blockProductionMethod: "central-scheduler",
+      fullSnapshotIntervalSlots: 1000,
+      noWaitForVoteToStartLeader: true,
+      noVoting: true,
+      entryPoint: networkInfo.entryPoint,
+      knownValidator: networkInfo.knownValidator,
+      expectedGenesisHash: networkInfo.genesisHash,
+    },
   },
   {
     dependsOn: [instance],
--- aws-validator-fd-ts/index.ts
+++ aws-validator-fd-ts/index.ts
@@ -6,59 +6,11 @@ const solanaConfig = new pulumi.Config("solana");
 // AWS-specific resources are created inside.
 import { sshKey, instance } from "./aws";
 
-// Provide some default options for known Solana networks.
-const networkOptions = {
-  devnet: {
-    gossip: {
-      entrypoints: [
-        "entrypoint.devnet.solana.com:8001",
-        "entrypoint2.devnet.solana.com:8001",
-        "entrypoint3.devnet.solana.com:8001",
-        "entrypoint4.devnet.solana.com:8001",
-        "entrypoint5.devnet.solana.com:8001",
-      ],
-    },
-    consensus: {
-      knownValidators: [
-        "dv1ZAGvdsz5hHLwWXsVnM94hWf1pjbKVau1QVkaMJ92",
-        "dv2eQHeP4RFrJZ6UeiZWoc3XTtmtZCUKxxCApCDcRNV",
-        "dv4ACNkpYPcE3aKmYDqZm9G5EB3J4MRoeE7WNDRBVJB",
-        "dv3qDFk1DTF36Z62bNvrCXe9sKATA6xvVy6A798xxAS",
-      ],
-      expectedGenesisHash: "EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG",
-    },
-  },
-  testnet: {
-    gossip: {
-      entrypoints: [
-        "entrypoint.testnet.solana.com:8001",
-        "entrypoint2.testnet.solana.com:8001",
-        "entrypoint3.testnet.solana.com:8001",
-      ],
-    },
-    consensus: {
-      knownValidators: [
-        "5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on",
-        "7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY",
-        "Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN",
-        "9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv",
-      ],
-      expectedGenesisHash: "4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY",
-    },
-  },
-};
-
-type NetworkName = keyof typeof networkOptions;
-
-function isValidName(name: string): name is NetworkName {
-  return Object.prototype.hasOwnProperty.call(networkOptions, name);
-}
-
-const networkName = solanaConfig.get("network") ?? "testnet";
-
-if (!isValidName(networkName)) {
-  throw new Error(`unknown Solana network '${networkName}' specified!`);
-}
+// Lookup information about the Solana network
+const networkName =
+  solanaConfig.get<svmkit.solana.NetworkName>("network") ??
+  svmkit.solana.NetworkName.Testnet;
+const networkInfo = svmkit.networkinfo.getNetworkInfoOutput({ networkName });
 
 // Create some keys for this validator to use.
 export const validatorKey = new svmkit.KeyPair("validator-key").json;
@@ -84,13 +36,14 @@ new svmkit.validator.Firedancer(
     config: {
       user: "sol",
       gossip: {
-        ...networkOptions[networkName].gossip,
         host: instance.publicIp,
+        entrypoints: networkInfo.entryPoint,
       },
       consensus: {
-        ...networkOptions[networkName].consensus,
         identityPath: "/home/sol/validator-keypair.json",
         voteAccountPath: "/home/sol/vote-account-keypair.json",
+        knownValidators: networkInfo.knownValidator,
+        expectedGenesisHash: networkInfo.genesisHash,
       },
       ledger: {
         path: "/home/sol/ledger",
